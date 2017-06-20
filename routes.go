f8042ace948fa9bf55c7261731af6a692dbb6756
arthur <madness@xakep.ru>
2017-06-20 22:30:12 +0300

    fix js for the plot

--- routes.go
+++ routes.go
@@ -4,7 +4,7 @@ func initializeRoutes() {
 
 	router.GET("/", showIndexPage)
 	router.POST("/upload", uploadFile)
-
+	router.Static("/static/", "static/")
 	fileRoutes := router.Group("/file")
 	{
 		fileRoutes.GET("/view/:file_id", getFile)
